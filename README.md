07a8e8e3bdcac46af2037e2fd8e34b7965e419ca
Phyremaster <phyremaster@hotmail.com>
2019-09-10 20:10:58 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # prop-hunt-plus
 A mod of a mod of a mod! This Garry's Mod addon expands the Prop Hunt gamemode.
+
+## Last Prop Standing
+The last prop alive will be given a magnum to shoot hunters with!
+
+## Prop Rotator
+Props can press `C` to set their horizontal rotation to the direction in which they are looking.
+
+## Bugfixes
+A few bugs from the original Prop Hunt have been patched, including incorrect win messages and broken maps in the mapvote.
---
+++ gamemodes/prop_hunt/gamemode/lps_init.lua
@@ -0,0 +1,30 @@
+GM.NoPlayerPlayerDamage = false
+
+resource.AddFile("sound/high_noon.mp3")
+
+function lastPropStandingTest(pl)
+	
+	local propCount = 0
+	for k, v in pairs(team.GetPlayers(TEAM_PROPS)) do
+		if v:Alive() then
+			propCount = propCount + 1
+		end
+	end
+	
+	if (pl:Team() == TEAM_PROPS) and (propCount == 1) then
+        
+		for k, v in pairs(team.GetPlayers(TEAM_PROPS)) do
+			if v:Alive() then
+				v:Give("weapon_357")
+				v:GiveAmmo(30, "357", true)
+				
+				PrintMessage(HUD_PRINTCENTER, "The last prop has been given a magnum!")
+                BroadcastLua("surface.PlaySound(\"high_noon.mp3\")")
+
+                return
+			end
+		end
+	end
+end
+hook.Add("PostPlayerDeath", "DeathLastPropStandingTest", lastPropStandingTest)
+hook.Add("PlayerDisconnected", "DisconnectLastPropStandingTest", lastPropStandingTest)
